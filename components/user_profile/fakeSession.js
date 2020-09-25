f340b2e4a484669032dba0943844d90911598fdd
marioa98 <mmartinez53@ucol.mx>
2020-09-25 13:42:06 -0500

    refactor: Migrate signup view

--- components/user_profile/fakeSession.js
+++ components/user_profile/fakeSession.js
@@ -1,7 +1,7 @@
 const session = {
   id: "94e305f9-6d57-4113-a490-977aab8564dd",
   authorization:
-  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwic2NwIjoidXNlciIsImF1ZCI6bnVsbCwiaWF0IjoxNjAxMDUxOTk3LCJleHAiOjE2MDExMzgzOTcsImp0aSI6IjRkZmM3NWNlLWNmMjAtNDc0MS1hZTVmLTI1MDdjZmU3Y2E3NiJ9.b4Dgodv2hX7ewxTSF3iY2llJcbvC7xncE-U8xyV08UI",
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwic2NwIjoidXNlciIsImF1ZCI6bnVsbCwiaWF0IjoxNjAxMDUxOTk3LCJleHAiOjE2MDExMzgzOTcsImp0aSI6IjRkZmM3NWNlLWNmMjAtNDc0MS1hZTVmLTI1MDdjZmU3Y2E3NiJ9.b4Dgodv2hX7ewxTSF3iY2llJcbvC7xncE-U8xyV08UI",
   names: "Juan",
   surnames: "Doe",
 };

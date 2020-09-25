26ae09cd1df0788840b02f4a26c2738ee27b4dba
marioa98 <mmartinez53@ucol.mx>
2020-09-25 09:37:32 -0500

    refactor: Delete hardcoded API connection URL

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,7 @@ import "../components/layout/DesktopNavbar";
 import axios from "axios";
 import MainLayout from "../layouts/MainLayout";
 
-axios.defaults.baseURL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 function MyApp({ Component, pageProps }) {
   return (

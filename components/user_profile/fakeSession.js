ea3cc66462a0782704a59b83964a4461cc3943ee
marioa98 <mmartinez53@ucol.mx>
2020-09-25 12:32:17 -0500

    refactor: Standarize ValidationsSchema methods location

--- components/user_profile/fakeSession.js
+++ components/user_profile/fakeSession.js
@@ -1,7 +1,7 @@
 const session = {
   id: "94e305f9-6d57-4113-a490-977aab8564dd",
   authorization:
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwic2NwIjoidXNlciIsImF1ZCI6bnVsbCwiaWF0IjoxNjAwOTY0OTYzLCJleHAiOjE2MDEwNTEzNjMsImp0aSI6IjIzZjY5NWY3LTBmZDktNGQ1My05MWE0LTFlOWRkOTJjOTU3NCJ9.lQmJ7kLfXZByFiVM97RxsTt39eJ4enhXPyop1D_3y2Q",
+  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwic2NwIjoidXNlciIsImF1ZCI6bnVsbCwiaWF0IjoxNjAxMDUxOTk3LCJleHAiOjE2MDExMzgzOTcsImp0aSI6IjRkZmM3NWNlLWNmMjAtNDc0MS1hZTVmLTI1MDdjZmU3Y2E3NiJ9.b4Dgodv2hX7ewxTSF3iY2llJcbvC7xncE-U8xyV08UI",
   names: "Juan",
   surnames: "Doe",
 };

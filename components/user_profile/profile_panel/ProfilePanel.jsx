6e164268656f07c832a259ca65c85284c8d99826
marioa98 <mmartinez53@ucol.mx>
2020-09-28 16:59:21 -0500

    feature: Access to profile sections through query param 'tab'

--- components/user_profile/profile_panel/ProfilePanel.jsx
+++ components/user_profile/profile_panel/ProfilePanel.jsx
@@ -1,15 +1,26 @@
+import { useRouter } from "next/router";
 import React, { useContext } from "react";
 import { Tab } from "semantic-ui-react";
 import { UserContext } from "../../../lib/context/UserContext";
+import { useCurrentTab } from "../../../lib/hooks/dataFormater";
 import panes from "./options/profilePanes";
 
 const ProfilePanel = () => {
   const { currentUser, token } = useContext(UserContext);
+  const router = useRouter();
+  const { currentIndex, validTabs } = useCurrentTab(router.query.tab);
+
+  const handleTabChange = (_event, data) => {
+    const { activeIndex } = data;
+    router.push(`/user/${router.query.userId}?tab=${validTabs[activeIndex]}`);
+  };
 
   return (
     <Tab
       menu={{ secondary: true, pointing: true }}
       panes={panes(currentUser.uuid, token)}
+      activeIndex={currentIndex}
+      onTabChange={handleTabChange}
     />
   );
 };

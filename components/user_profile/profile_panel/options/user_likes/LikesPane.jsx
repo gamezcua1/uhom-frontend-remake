d17b038f2e1a1565225d0292f27790290ecd81c4
marioa98 <mmartinez53@ucol.mx>
2020-09-28 13:56:29 -0500

    feature (WIP): Add user likes panel

---
+++ components/user_profile/profile_panel/options/user_likes/LikesPane.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { Tab } from "semantic-ui-react";
+
+import PropertiesCatalog from "../../../../properties/catalog/PropertiesCatalog";
+import { useUserLikes } from "../../../../../lib/hooks/users";
+
+const LikesPane = () => {
+  const noResultMessage =
+    "Aún no has dado 'like' a ninguna propiedad. Aquí aparecerán todas las propiedades que te gustan";
+  const router = useRouter();
+  const [page, setPage] = useState(router.query.page || 1);
+  const { properties, itemsPerPage, totalItems, response } = useUserLikes(
+    router.query.userId,
+    { page }
+  );
+
+  console.log(properties);
+
+  return (
+    <Tab.Pane>
+      <PropertiesCatalog
+        currentPage={page}
+        itemsPerPage={itemsPerPage}
+        noResultMessage={noResultMessage}
+        properties={properties}
+        setCurrentPage={setPage}
+        response={response}
+        totalItems={totalItems}
+      />
+    </Tab.Pane>
+  );
+};
+
+export default LikesPane;

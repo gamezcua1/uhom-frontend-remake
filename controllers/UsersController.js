373742a656343c7ca2733cc9362d2d47c3c3cc7f
marioa98 <mmartinez53@ucol.mx>
2020-09-27 21:51:18 -0500

    fix: Solve typo in UsersController params

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,11 +2,7 @@ import axios from "axios";
 import { setHeaders, setUserParams } from "../lib/helpers/requestFormatter";
 
 export default class UsersController {
-  static update(uuid, user, token) {
-    return axios.patch(
-      `/users/${uuid}`,
-      setUserParams(user),
-      setHeaders(token)
-    );
+  static update(uuid, user) {
+    return axios.patch(`/users/${uuid}`, setUserParams(user), setHeaders());
   }
 }

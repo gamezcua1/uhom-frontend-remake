f1b287f285452bc26bfaaff77d7e4e3a99bc07fa
marioa98 <mmartinez53@ucol.mx>
2020-09-30 11:38:10 -0500

    feature: Add footer

--- layouts/MainLayout.jsx
+++ layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import DesktopNavbar from "../components/layout/DesktopNavbar";
+import Footer from "../components/layout/Footer";
 import { UserContextProvider } from "../lib/context/UserContext";
 
 const MainLayout = ({ children }) => (
   <UserContextProvider>
     <DesktopNavbar />
     {children}
+    <Footer />
   </UserContextProvider>
 );
 

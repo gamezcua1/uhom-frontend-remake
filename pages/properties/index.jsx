201e440af0c31fcf5eda2f17b94a2e96909e50a3
marioa98 <mmartinez53@ucol.mx>
2020-09-30 10:20:10 -0500

    refactor: Add hook to handle pages

--- pages/properties/index.jsx
+++ pages/properties/index.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 
 import { usePropertiesIndex } from "../../lib/hooks/properties";
 import PropertiesCatalog from "../../components/properties/catalog/PropertiesCatalog";
+import { useCurrentPage } from "../../lib/hooks/pagination";
 
 const PropertiesIndex = () => {
   const noResultMessage =
     "No se encuentraron propiedades con las características especificadas.";
   const router = useRouter();
-  const [page, setPage] = useState(router.query.page || 1);
+  const [page, setPage] = useCurrentPage(router.query.page);
   const { properties, itemsPerPage, totalItems, response } = usePropertiesIndex(
     {
       page,

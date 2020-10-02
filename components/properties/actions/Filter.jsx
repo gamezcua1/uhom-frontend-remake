f7594b9200df7cfe62cc90c76f5e1af463936226
Gerald Amezcua <gerald.amezcua@michelada.io>
2020-10-02 07:54:52 -0500

    fix icon color and added ranges into filter bar

--- components/properties/actions/Filter.jsx
+++ components/properties/actions/Filter.jsx
@@ -15,16 +15,19 @@ const locations = [
   "Villa",
 ].map((e, i) => ({ key: `location_${i}`, value: i, text: e }));
 
-const square_meters = [0, 500, 1000, 5000, 10000].map((e) => ({
+const square_meters = [0, 100, 200, 300].map((e) => ({
   key: `square_${e}`,
   value: e,
   text: e === 0 ? "Todos" : e,
 }));
-const price = ["0", "500,000", "1,000,000", "1,500,500"].map((e) => ({
-  key: `price_${e}`,
-  value: parseInt(e.split(",").join("")),
-  text: e == "0" ? "Todos" : e,
-}));
+
+const price = ["0", "500,000", "1,000,000", "1,500,000", "2,000,000"].map(
+  (e) => ({
+    key: `price_${e}`,
+    value: parseInt(e.split(",").join("")),
+    text: e == "0" ? "Todos" : e,
+  })
+);
 
 const rooms = [0, 1, 2, 3, 4, 5, 6].map((_p, i) => ({
   key: `room_${i}`,
--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import "semantic-ui-css/semantic.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import "../components/layout/DesktopNavbar";
 import axios from "axios";
 import MainLayout from "../layouts/MainLayout";
 

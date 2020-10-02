b508c65ac4d3ca299806eaec7b2797303ce4ed21
marioa98 <mmartinez53@ucol.mx>
2020-10-02 17:30:25 -0500

    test: Add test cases for login

--- cypress/support/commands/login.js
+++ cypress/support/commands/login.js
@@ -2,16 +2,13 @@ Cypress.Commands.add("visitLogin", () => {
   cy.get("button.btn-login").contains("Iniciar sesión").click();
 });
 
-Cypress.Commands.add("loginAsUser", (invalidUser = {}) => {
+Cypress.Commands.add("loginAsUser", (validLogin = true) => {
   cy.fixture("users/user.json").then((user) => {
-    const { email, password } = invalidUser;
-
-    cy.get("#email").type(email || user.email);
-    cy.get("#password").type(password || user.password);
+    validLogin ? cy.mockLogin() : cy.mockInvalidLogin();
+    cy.get("#email").type(user.email);
+    cy.get("#password").type(user.password);
     cy.get(".fluid > button").click();
   });
 });
 
-Cypress.Commands.add("invalidLogin", (invalidUser) =>
-  cy.loginAsUser(invalidUser)
-);
+Cypress.Commands.add("invalidLogin", () => cy.loginAsUser(false));

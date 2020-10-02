952dc6200539a0c457033b6acd0b0755f8283a47
marioa98 <mmartinez53@ucol.mx>
2020-10-02 13:52:54 -0500

    test (WIP): Add e2e test cases for login

--- cypress/integration/e2e/sessions/login.test.js
+++ cypress/integration/e2e/sessions/login.test.js
@@ -1,1 +1,37 @@
-describe("Loging", () => {});
+describe("Login", () => {
+  context("Show form", () => {
+    it("Should display the form if the user is not logged in", () => {
+      cy.visit("/");
+      cy.get("button.btn-login").contains("Iniciar sesión").click();
+      cy.get("form#login").should("be.visible");
+    });
+  });
+
+  context("Login as User", () => {
+    beforeEach(() => {
+      cy.visit("/");
+      cy.visitLogin();
+    });
+
+    it("Should mark as required when submit with both fields are empty", () => {
+      cy.get('button[type="submit"]').click();
+
+      const errors = cy.get("p.dark-error");
+      errors.should("have.length", 2);
+      errors.should("have.html", "⚠ Este campo es obligatorio");
+    });
+
+    it("Should mark error when credentials are incorrect", () => {
+      cy.invalidLogin({ password: "Invalidpassw0rd" });
+      cy.get("p.dark-error").should(
+        "have.html",
+        "La combinación de email y contraseña es incorrecta"
+      );
+    });
+
+    it("Should be a successful login", () => {
+      cy.loginAsUser();
+      cy.get("span#avatar").should("be.visible");
+    });
+  });
+});
---
+++ cypress/support/commands/mocks/sessions.js
@@ -0,0 +1,13 @@
+Cypress.Commands.add("mockLogin", (params) => {
+  cy.server();
+  cy.route({
+    method: "POST",
+    url: "/login",
+    body: params,
+    response: "fixture:users/loggedUser.json",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMiIsInNjcCI6InVzZXIiLCJhdWQiOm51bGwsImlhdCI6MTYwMTY2MDM3NCwiZXhwIjoxNjA0MjkwMTIwLCJqdGkiOiIwZDhkZjMxNC1kNjg2LTQ2Y2MtYmNkNy1mODI5MmM0ZTljNTQifQ.3TRo8l8YKOBh9_Z7FmgruRI5iDSVvVzNOGjcgIZ5dXw",
+    },
+  });
+});

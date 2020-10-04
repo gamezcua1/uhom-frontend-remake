644b1c42667164a9d07a9948e2f89e337b5b00a9
marioa98 <mmartinez53@ucol.mx>
2020-10-04 15:08:12 -0500

    test: Refactor with async requests

--- cypress/integration/e2e/properties/show.spec.js
+++ cypress/integration/e2e/properties/show.spec.js
@@ -1,24 +1,19 @@
 describe("End-2-end tests for properties show", () => {
   beforeEach(() => {
     cy.visit("/");
-    cy.visitProperties();
+    cy.visitSingleProperty();
   });
 
   it("Can navigate to a property detail", () => {
-    cy.get(".extra.center.aligned.content > a").first().click();
-    cy.loadSingleProperty();
     cy.get(".content").contains("¡Haz una cita, visita la casa!");
   });
 
   it("Cannot ask for a tour if user is not logged in", () => {
-    cy.get(".extra.center.aligned.content > a").first().click();
-    cy.loadSingleProperty();
     cy.get("#appoinmentButton").click();
     cy.get("body").contains("¡Ups! Parece que aún no te has registrado");
   });
 
   it("Can ask for a tour if user is logged in", () => {
-    cy.visitLogin();
     cy.loginAsUser();
 
     cy.get(".extra.center.aligned.content > a").first().click();

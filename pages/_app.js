78c2fd4eb6b4a10a31e9b5b9115dc97d914405c8
marioa98 <mmartinez53@ucol.mx>
2020-10-05 10:31:29 -0500

    feature: Add page title

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import React from "react";
-import "../styles/index.scss";
+import Head from "next/head";
 import "semantic-ui-css/semantic.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+import "../styles/index.scss";
 
 import axios from "axios";
 import MainLayout from "../layouts/MainLayout";
@@ -11,9 +13,15 @@ axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <>
+      <Head>
+        <title>GoHome&apos;s - Es tiempo de ir a casa</title>
+      </Head>
+
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
   );
 }
 

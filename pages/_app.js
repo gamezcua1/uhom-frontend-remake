f32b1910071ee2a3e622dea5db4decbd6977016a
marioa98 <mmartinez53@ucol.mx>
2020-10-05 12:05:46 -0500

    feature: Convert into PWA

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,16 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta charSet="utf8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+
         <title>GoHome&apos;s - Es tiempo de ir a casa</title>
       </Head>
 

a7e898d296e17c5e97dfbfe2190f2c1179ed33d5
marioa98 <mmartinez53@ucol.mx>
2020-10-05 12:05:46 -0500

    feature: Convert into PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const path = require("path");
+const withPWA = require("next-pwa");
 
-module.exports = {
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-};
+});

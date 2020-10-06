7b0f80eb390c02303255560c352509eccc3eeeca
marioa98 <mmartinez53@ucol.mx>
2020-10-06 13:25:25 -0500

    feature: Migrate carousel to react-image-gallery

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Head from "next/head";
 import "semantic-ui-css/semantic.min.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 import "../styles/index.scss";
 

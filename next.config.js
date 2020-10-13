5f7eca613e7cfd52112a4bd4fdcf646f7190ab6f
marioa98 <mmartinez53@ucol.mx>
2020-10-12 22:41:02 -0500

    Add PWA scope when is build

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
 const path = require("path");
-// const withPWA = require("next-pwa");
+const withPWA = require("next-pwa");
 
-// module.exports = withPWA({
-//   pwa: {
-//     dest: "public",
-//   },
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, "styles")],
-//   },
-// });
-
-module.exports = {
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    scope: "/app",
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-};
+});

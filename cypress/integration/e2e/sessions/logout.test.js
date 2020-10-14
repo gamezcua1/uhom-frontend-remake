9921ff94f3eaccc4fc350e28dffc151fb35fb226
marioa98 <mmartinez53@ucol.mx>
2020-10-14 12:46:13 -0500

    Deletes cookies after logout

--- cypress/integration/e2e/sessions/logout.test.js
+++ cypress/integration/e2e/sessions/logout.test.js
@@ -1,3 +1,4 @@
+/// <reference types="cypress"/>
 describe("Logout method", () => {
   beforeEach(() => {
     cy.visit("/");
@@ -9,6 +10,17 @@ describe("Logout method", () => {
       cy.get("span#loggedUserAvatar").first().should("exist");
       cy.logoutAsUser();
       cy.get("body").contains("Iniciar sesión");
+    });
+  });
+
+  it("Should delete cookies successfully after logout", () => {
+    cy.fixture("users.json").then(({ client }) => {
+      cy.login(client);
+      cy.wait(500);
+      cy.getCookie("uid").should("exist");
+      cy.logoutAsUser();
+      cy.wait(500);
+      cy.getCookie("uid").should("not.exist");
     });
   });
 });

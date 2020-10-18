a04bd9a2ff60947b914939b31c95bad778520078
marioa98 <mmartinez53@ucol.mx>
2020-10-18 18:00:59 -0500

    Disable PWA config when NODE_ENV === 'dev'

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
 const path = require("path");
 // const withPWA = require("next-pwa");
 
-// module.exports = withPWA({
-//   pwa: {
-//     dest: "public",
-//   },
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, "styles")],
-//   },
-// });
-
-module.exports = {
+module.exports = withPWA({
+  pwa: {
+    disable: process.env.NODE_ENV === "development",
+    dest: "public",
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-};
+});
